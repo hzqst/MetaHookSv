bfa7f9435bb303ae1ae99f413a626e1585556e17
hzqst <113660872@qq.com>
2021-08-02 18:19:09 +0800

    Change DPI-awareness to per-monitor DPI-awareness for #43.

---
+++ SteamAppsLocation/SteamAppsLocation/SteamAppsLocation.cpp
@@ -0,0 +1,26 @@
+﻿#include <Windows.h>
+#include "steam_api.h"
+
+int main(int argc, const char **argv)
+{
+	if (argc < 2)
+		return 0;
+
+	if (SteamAPI_Init())
+	{
+		if (SteamAPI_IsSteamRunning())
+		{
+			int appId = atoi(argv[1]);
+
+			char szAppInstallDir[1024] = {0};
+			if (SteamApps()->GetAppInstallDir(appId, szAppInstallDir, sizeof(szAppInstallDir)))
+			{
+				puts(szAppInstallDir);
+			}
+		}
+
+		SteamAPI_Shutdown();
+	}
+
+	return 0;
+}
