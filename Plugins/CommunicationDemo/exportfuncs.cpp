7d8854c18ddf0d70969d7f98417a6274337e2733
hzqst <113660872@qq.com>
2021-11-02 15:17:03 +0800

    CommunicationDemo

--- Plugins/CommunicationDemo/exportfuncs.cpp
+++ Plugins/CommunicationDemo/exportfuncs.cpp
@@ -14,8 +14,9 @@ engine_studio_api_t IEngineStudio;
 r_studio_interface_t **gpStudioInterface;
 
 #define MHSV_CMD_QUERY_PLUGIN 1
+#define MHSV_CMD_QUERY_CVAR 2
 
-int __MsgFunc_MetaHookSv(const char *pszName, int iSize, void *pbuf)
+void MsgFunc_MetaHookSv(void)
 {
 	auto cmd2 = READ_BYTE();
 	if (cmd2 == MHSV_CMD_QUERY_PLUGIN)
@@ -24,10 +25,34 @@ int __MsgFunc_MetaHookSv(const char *pszName, int iSize, void *pbuf)
 		for (int index = -1; g_pMetaHookAPI->QueryPluginInfo(index, &info); ++index)
 		{
 			char cmd[128] = { 0 };
-			snprintf(cmd, sizeof(cmd) - 1, "mh_plugin %d %d \"%s\" \"%s\" ", info.Index, info.InterfaceVersion, info.PluginName, info.PluginVersion);
+			snprintf(cmd, sizeof(cmd) - 1, "mh_reportplugin %d %d \"%s\" \"%s\" ", info.Index, info.InterfaceVersion, info.PluginName, info.PluginVersion);
 			gEngfuncs.pfnServerCmd(cmd);
 		}
 	}
+	else if (cmd2 == MHSV_CMD_QUERY_CVAR)
+	{
+		auto request_id = READ_LONG();
+		auto cvar_name = READ_STRING();
+		auto cvar = gEngfuncs.pfnGetCvarPointer(cvar_name);
+		if (cvar)
+		{
+			char cmd[128] = { 0 };
+			snprintf(cmd, sizeof(cmd) - 1, "mh_reportcvar %d \"%s\" \"%s\" ", request_id, cvar->name, cvar->string);
+			gEngfuncs.pfnServerCmd(cmd);
+		}
+		else
+		{
+			char cmd[128] = { 0 };
+			snprintf(cmd, sizeof(cmd) - 1, "mh_reportcvar %d \"%s\"", request_id, cvar_name);
+			gEngfuncs.pfnServerCmd(cmd);
+		}
+	}
+}
+
+int __MsgFunc_MetaHookSv(const char *pszName, int iSize, void *pbuf)
+{
+	BEGIN_READ(pbuf, iSize);
+	MsgFunc_MetaHookSv();
 	return 0;
 }
 
@@ -39,19 +64,7 @@ void HUD_DirectorMessage(int iSize, void *pbuf)
 
 	if (cmd == 100)
 	{
-		auto cmd2 = READ_BYTE();
-
-		if (cmd2 == MHSV_CMD_QUERY_PLUGIN)
-		{
-			mh_plugininfo_t info;
-			for (int index = -1; g_pMetaHookAPI->QueryPluginInfo(index, &info); ++index)
-			{
-				char cmd[128] = { 0 };
-				snprintf(cmd, sizeof(cmd) - 1, "mh_plugin %d %d \"%s\" \"%s\" ", info.Index, info.InterfaceVersion, info.PluginName, info.PluginVersion);
-				gEngfuncs.pfnServerCmd(cmd);
-			}
-		}
-
+		MsgFunc_MetaHookSv();
 		return;
 	}	
 
