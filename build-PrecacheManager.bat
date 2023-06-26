0a4ada8da9000d9630d4701f07b82debf6565a31
hzqst <113660872@qq.com>
2023-06-26 14:48:24 +0800

    1

---
+++ build-PrecacheManager.bat
@@ -0,0 +1,12 @@
+cd /d "%~dp0"
+
+for /f "usebackq tokens=*" %%i in (`vswhere -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath`) do (
+  set InstallDir=%%i
+)
+
+if exist "%InstallDir%\Common7\Tools\vsdevcmd.bat" (
+
+    "%InstallDir%\Common7\Tools\vsdevcmd.bat" -arch=x86
+
+    MSBuild.exe "Plugins/PrecacheManager/PrecacheManager.vcxproj" /p:Configuration=Release /p:Platform="Win32"
+)
