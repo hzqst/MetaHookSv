ec4ff8e791e127aa9f93f2ce47ff42491ac30e5e
hzqst <113660872@qq.com>
2024-01-28 23:39:04 +0800

    1

---
+++ include/Interface/VGUI/IClientPanel.h
@@ -0,0 +1,194 @@
+//========= Copyright ?1996-2005, Valve Corporation, All rights reserved. ============//
+//
+// Purpose: 
+//
+// $NoKeywords: $
+//=============================================================================//
+
+#ifndef ICLIENTPANEL_H
+#define ICLIENTPANEL_H
+
+#ifdef _WIN32
+#pragma once
+#endif
+
+#include "VGUI.h"
+#include "MessageMap.h"
+#include "Color.h"
+#include "KeyCode.h"
+#include "MouseCode.h"
+
+#ifdef GetClassName
+#undef GetClassName
+#endif
+
+class KeyValues;
+
+namespace vgui
+{
+
+class Panel;
+class BuildGroup;
+class SurfaceBase;
+class IBorder;
+class IScheme;
+
+enum EInterfaceID
+{
+	ICLIENTPANEL_STANDARD_INTERFACE = 0,
+};
+
+//-----------------------------------------------------------------------------
+// Purpose: Interface from vgui panels -> Client panels
+//			This interface cannot be changed without rebuilding all vgui projects
+//			Primarily this interface handles dispatching messages from core vgui to controls
+//			The additional functions are all their for debugging or optimization reasons
+//			To add to this later, use QueryInterface() to see if they support new interfaces
+//-----------------------------------------------------------------------------
+class IClientPanel
+{
+public:
+	virtual VPANEL GetVPanel() = 0;//0
+	virtual void Think() = 0;//1
+	virtual void PerformApplySchemeSettings() = 0;//2
+	virtual void PaintTraverse(bool forceRepaint, bool allowForce) = 0;//3
+	virtual void Repaint() = 0;//4
+	virtual VPANEL IsWithinTraverse(int x, int y, bool traversePopups) = 0;//5
+	virtual void GetInset(int &top, int &left, int &right, int &bottom) = 0;//6
+	virtual void GetClipRect(int &x0, int &y0, int &x1, int &y1) = 0;//7
+	virtual void OnChildAdded(VPANEL child) = 0;//8
+	virtual void OnSizeChanged(int newWide, int newTall) = 0;//9
+	virtual void InternalFocusChanged(bool lost) = 0;//10
+	virtual bool RequestInfo(KeyValues *outputData) = 0;//11
+	virtual void RequestFocus(int direction) = 0;//12
+	virtual bool RequestFocusPrev(VPANEL existingPanel) = 0;//13
+	virtual bool RequestFocusNext(VPANEL existingPanel) = 0;//14
+	virtual void OnMessage(const KeyValues *params, VPANEL ifromPanel) = 0;//15
+	virtual VPANEL GetCurrentKeyFocus() = 0;//16
+	virtual int GetTabPosition() = 0;//17
+	virtual const char *GetName() = 0;//18
+	virtual const char *GetClassName() = 0;//19
+	virtual HScheme GetScheme() = 0;//20
+	virtual bool IsProportional() = 0;//21
+	virtual bool IsAutoDeleteSet() = 0;//22
+	virtual void DeletePanel() = 0;//23
+	virtual void *QueryInterface(EInterfaceID id) = 0;//24
+	virtual Panel *GetPanel() = 0;//25
+	virtual const char *GetModuleName() = 0;//26
+	virtual PanelMessageMap* GetMessageMap() = 0;//27
+	virtual void IPanel_dtor() = 0;//28
+#if !defined(_WIN32) && !defined(_WIN64)
+	virtual void IPanel_dtor2() = 0;//29
+#endif
+	virtual void SetVisible(bool param_1) = 0;//29
+	virtual bool IsVisible() = 0;//30
+	virtual void PostMessage1(Panel* target, KeyValues* message, float delaySeconds) = 0;//31
+	virtual void PostMessage2(VPANEL target, KeyValues* message, float delaySeconds) = 0;//32
+	virtual void OnMove() = 0;//33
+	virtual Panel* GetParent() = 0;//34
+	virtual VPANEL GetVParent() = 0;//35
+	virtual void SetParent(Panel* param_1) = 0;//36
+	virtual void SetParent(VPANEL param_1) = 0;//37
+	virtual bool HasParent(VPANEL potentialParent) = 0;//38
+	virtual void SetAutoDelete(bool param_1) = 0;//39
+	virtual void AddActionSignalTarget2(VPANEL vpanel) = 0;//40
+	virtual void AddActionSignalTarget(Panel* panel) = 0;//41
+	virtual void RemoveActionSignalTarget(Panel* panel) = 0;//42
+	virtual void PostActionSignal(KeyValues* keyvalue) = 0;//43
+	virtual bool RequestInfoFromChild(const char* childName, KeyValues* outputData) = 0;//44
+	virtual void PostMessageToChild(const char* param_1, KeyValues* param_2) = 0;//45
+	virtual bool SetInfo(KeyValues* inputData) = 0;//46
+	virtual void SetEnabled(bool state) = 0;//47
+	virtual bool IsEnabled() = 0;//48
+	virtual bool IsPopup() = 0;//49
+	virtual void MoveToFront() = 0;//50
+	virtual void SetBgColor(Color param_1) = 0;//51
+	virtual void SetFgColor(Color param_1) = 0;//52
+	virtual Color GetBgColor() = 0;//53
+	virtual Color GetFgColor() = 0;//54
+	virtual void SetCursor(HCursor param_1) = 0;//55
+	virtual HCursor GetCursor() = 0;//56
+	virtual bool HasFocus() = 0;//57
+	virtual void InvalidateLayout(bool param_1, bool param_2) = 0;//58
+	virtual void SetTabPosition(int param_1) = 0;//59
+	virtual void SetBorder(IBorder* param_1) = 0;//60
+	virtual IBorder* GetBorder() = 0;//61
+	virtual void SetPaintBorderEnabled(bool param_1) = 0;//62
+	virtual void SetPaintBackgroundEnabled(bool param_1) = 0;//63
+	virtual void SetPaintEnabled(bool param_1) = 0;//64
+	virtual void SetPostChildPaintEnabled(bool param_1) = 0;//65
+	virtual void GetPaintSize(int& param_1, int& param_2) = 0;//66
+	virtual void SetBuildGroup(BuildGroup* param_1) = 0;//67
+	virtual bool IsBuildGroupEnabled() = 0;//68
+	virtual bool IsCursorNone() = 0;//69
+	virtual bool IsCursorOver() = 0;//70
+	virtual void MarkForDeletion() = 0;//71
+	virtual bool IsLayoutInvalid() = 0;//72
+	virtual Panel *HasHotkey(wchar_t key) = 0;//73
+	virtual bool IsOpaque() = 0;//74
+	virtual void SetScheme(HScheme param_1) = 0;//76
+	virtual void SetScheme2(const char* param_1) = 0;//75
+	virtual Color GetSchemeColor(const char* keyName, IScheme* pScheme) = 0;//77
+	virtual Color GetSchemeColor2(const char* keyName, Color defaultColor, IScheme* pScheme) = 0;//78
+	virtual void ApplySchemeSettings(IScheme* pScheme) = 0;//79
+	virtual void ApplySettings(KeyValues* inResourceData) = 0;//80
+	virtual void GetSettings(KeyValues* param_1) = 0;//81
+	virtual const char* GetDescription() = 0;//82
+	virtual void ApplyUserConfigSettings(KeyValues* userConfig) = 0;//83
+	virtual void GetUserConfigSettings(KeyValues* userConfig) = 0;//84
+	virtual bool HasUserConfigSettings() = 0;//85
+	virtual void OnThink() = 0;//86
+	virtual void OnCommand(const char* command) = 0;//87
+	virtual void OnMouseCaptureLost() = 0;//88
+	virtual void OnSetFocus() = 0;//89
+	virtual void OnKillFocus() = 0;//90
+	virtual void OnDelete() = 0;//91
+	virtual void OnTick() = 0;//92
+	virtual void OnCursorMoved(int x, int y) = 0;//93
+	virtual void OnCursorEntered() = 0;//94
+	virtual void OnCursorExited() = 0;//95
+	virtual void OnMousePressed(MouseCode code) = 0;//96
+	virtual void OnMouseDoublePressed(MouseCode code) = 0;//97
+	virtual void OnMouseReleased(MouseCode code) = 0;//98
+	virtual void OnMouseWheeled(int delta) = 0;//99
+	virtual void OnKeyCodePressed(KeyCode code) = 0;//100
+	virtual void OnKeyCodeTyped(KeyCode code) = 0;//101
+	virtual void OnKeyTyped(wchar_t unichar) = 0;//102
+	virtual void OnKeyCodeReleased(KeyCode code) = 0;//103
+	virtual void OnKeyFocusTicked() = 0;//104
+	virtual void OnMouseFocusTicked() = 0;//105
+	virtual void PaintBackground() = 0;//106
+	virtual void Paint() = 0;//107
+	virtual void PaintBorder() = 0;//108
+	virtual void PaintBuildOverlay() = 0;//109
+	virtual void PostChildPaint() = 0;//110
+	virtual void PerformLayout() = 0;//111
+	virtual PanelMap_t *GetPanelMap() = 0;//112
+	virtual void SetProportional(bool state) = 0;//113
+	virtual void SetMouseInputEnabled(bool state) = 0;//114
+	virtual void SetKeyBoardInputEnabled(bool state) = 0;//115
+	virtual bool IsMouseInputEnabled() = 0;//116
+	virtual bool IsKeyBoardInputEnabled() = 0;//117
+	virtual void OnRequestFocus(VPANEL subFocus, VPANEL defaultPanel) = 0;//118
+	virtual void InternalCursorMoved(int x, int y) = 0;//119
+	virtual void InternalCursorEntered() = 0;//120
+	virtual void InternalCursorExited() = 0;//121
+	virtual void InternalMousePressed(int code) = 0;//122
+	virtual void InternalMouseDoublePressed(int code) = 0;//123
+	virtual void InternalMouseReleased(int code) = 0;//124
+	virtual void InternalMouseWheeled(int code) = 0;//125
+	virtual void InternalKeyCodePressed(int code) = 0;//126
+	virtual void InternalKeyCodeTyped(int code) = 0;//127
+	virtual void InternalKeyTyped(int code) = 0;//128
+	virtual void InternalKeyCodeReleased(int code) = 0;//129
+	virtual void InternalKeyFocusTicked() = 0;//130
+	virtual void InternalMouseFocusTicked() = 0;//131
+	virtual void InternalInvalidateLayout() = 0;//132
+	virtual void InternalMove() = 0;//133
+
+};
+
+} // namespace vgui
+
+
+#endif // ICLIENTPANEL_H
---
+++ include/Interface/VGUI/ISurface2.h
@@ -0,0 +1,254 @@
+#ifndef ISURFACE2_H
+#define ISURFACE2_H
+
+#include <interface.h>
+#include "VGUI.h"
+#include "IHTML.h"
+#include <mathlib/vector2d.h>
+#include <Color.h>
+
+namespace vgui
+{
+
+class IImage;
+
+// SRC only defines
+
+struct Vertex_t
+{
+	Vertex_t() {}
+	Vertex_t( const Vector2D &pos, const Vector2D &coord = Vector2D( 0, 0 ) )
+	{
+		m_Position = pos;
+		m_TexCoord = coord;
+	}
+	void Init( const Vector2D &pos, const Vector2D &coord = Vector2D( 0, 0 ) )
+	{
+		m_Position = pos;
+		m_TexCoord = coord;
+	}
+	
+	Vector2D	m_Position;
+	Vector2D	m_TexCoord;
+};
+
+enum FontDrawType_t
+{
+	// Use the "additive" value from the scheme file
+	FONT_DRAW_DEFAULT = 0,
+
+	// Overrides
+	FONT_DRAW_NONADDITIVE,
+	FONT_DRAW_ADDITIVE,
+
+	FONT_DRAW_TYPE_COUNT = 2,
+};
+
+// Refactor these two
+struct CharRenderInfo
+{
+	// In:
+	FontDrawType_t	drawType;
+	wchar_t			ch;
+
+	// Out
+	bool			valid;
+
+	// In/Out (true by default)
+	bool			shouldclip;
+	// Text pos
+	int				x, y;
+	// Top left and bottom right
+	Vertex_t		verts[ 2 ];
+	int				textureId;
+	int				abcA;
+	int				abcB;
+	int				abcC;
+	int				fontTall;
+	HFont			currentFont;
+};
+
+struct IntRect
+{
+	int x0;
+	int y0;
+	int x1;
+	int y1;
+};
+
+
+#ifndef VGUI2_SURFACE_FEATURE
+#define VGUI2_SURFACE_FEATURE
+// returns true if the surface supports minimize & maximize capabilities
+enum SurfaceFeature_e
+{
+	ANTIALIASED_FONTS = 1,
+	DROPSHADOW_FONTS = 2,
+	ESCAPE_KEY = 3,
+	OPENING_NEW_HTML_WINDOWS = 4,
+	FRAME_MINIMIZE_MAXIMIZE = 5,
+	DIRECT_HWND_RENDER = 6,
+	OUTLINE_FONTS = 7,
+};
+
+#endif
+
+//Simple wrapper around original g_pSurface and g_pSurface_HL25
+
+class ISurface2 : public IBaseInterface
+{
+public:
+
+	// adds to the font
+	enum EFontFlags
+	{
+		FONTFLAG_NONE,
+		FONTFLAG_ITALIC = 0x001,
+		FONTFLAG_UNDERLINE = 0x002,
+		FONTFLAG_STRIKEOUT = 0x004,
+		FONTFLAG_SYMBOL = 0x008,
+		FONTFLAG_ANTIALIAS = 0x010,
+		FONTFLAG_GAUSSIANBLUR = 0x020,
+		FONTFLAG_ROTARY = 0x040,
+		FONTFLAG_DROPSHADOW = 0x080,
+		FONTFLAG_ADDITIVE = 0x100,
+		FONTFLAG_OUTLINE = 0x200,
+		FONTFLAG_CUSTOM = 0x400,
+		FONTFLAG_OUTLINE2 = 0x800,
+	};
+
+public:
+	virtual void Shutdown( void ) = 0;
+	virtual void RunFrame( void ) = 0;
+	virtual VPANEL GetEmbeddedPanel( void ) = 0;
+	virtual void SetEmbeddedPanel( VPANEL pPanel ) = 0;
+	virtual void PushMakeCurrent( VPANEL panel, bool useInsets ) = 0;
+	virtual void PopMakeCurrent( VPANEL panel ) = 0;
+	virtual void DrawSetColor( int r, int g, int b, int a ) = 0;
+	virtual void DrawSetColor( Color col ) = 0;
+	virtual void DrawFilledRect( int x0, int y0, int x1, int y1 ) = 0;
+	virtual void DrawFilledRectArray( IntRect *pRects, int numRects ) = 0;
+	virtual void DrawOutlinedRect( int x0, int y0, int x1, int y1 ) = 0;
+	virtual void DrawLine( int x0, int y0, int x1, int y1 ) = 0;
+	virtual void DrawPolyLine( int *px, int *py, int numPoints ) = 0;
+	virtual void DrawSetTextFont( HFont font ) = 0;
+	virtual void DrawSetTextColor( int r, int g, int b, int a ) = 0;
+	virtual void DrawSetTextColor( Color col ) = 0;
+	virtual void DrawSetTextPos( int x, int y ) = 0;
+	virtual void DrawGetTextPos( int &x, int &y ) = 0;
+	virtual void DrawPrintText( const wchar_t *text, int textLen, FontDrawType_t drawType = FONT_DRAW_DEFAULT ) = 0;
+	virtual void DrawUnicodeChar( wchar_t wch, FontDrawType_t drawType = FONT_DRAW_DEFAULT ) = 0;
+	virtual void DrawFlushText( void ) = 0;
+	virtual IHTML *CreateHTMLWindow( IHTMLEvents *events, VPANEL context ) = 0;
+	virtual void PaintHTMLWindow( IHTML *htmlwin ) = 0;
+	virtual void DeleteHTMLWindow( IHTML *htmlwin ) = 0;
+	virtual int DrawGetTextureId( const char *filename ) = 0;
+	virtual bool DrawGetTextureFile( int id, char *filename, int maxlen ) = 0;
+	virtual void DrawSetTextureFile( int id, const char *filename, int hardwareFilter, bool forceReload ) = 0;
+	virtual void DrawSetTextureRGBA( int id, const unsigned char *rgba, int wide, int tall, int hardwareFilter, bool forceReload ) = 0;
+	virtual void DrawSetTexture( int id ) = 0;
+	virtual void DrawGetTextureSize( int id, int &wide, int &tall ) = 0;
+	virtual void DrawTexturedRect( int x0, int y0, int x1, int y1 ) = 0;
+	virtual void DrawTexturedRectAdd(int x0, int y0, int x1, int y1) = 0;
+	virtual bool IsTextureIDValid( int id ) = 0;
+	virtual int CreateNewTextureID( bool procedural = false ) = 0;
+	virtual void GetScreenSize( int &wide, int &tall ) = 0;
+	virtual void SetAsTopMost( VPANEL panel, bool state ) = 0;
+	virtual void BringToFront( VPANEL panel ) = 0;
+	virtual void SetForegroundWindow( VPANEL panel ) = 0;
+	virtual void SetPanelVisible( VPANEL panel, bool state ) = 0;
+	virtual void SetMinimized( VPANEL panel, bool state ) = 0;
+	virtual bool IsMinimized( VPANEL panel ) = 0;
+	virtual void FlashWindow( VPANEL panel, bool state ) = 0;
+	virtual void SetTitle( VPANEL panel, const wchar_t *title ) = 0;
+	virtual void SetAsToolBar( VPANEL panel, bool state ) = 0;
+	virtual void CreatePopup( VPANEL panel, bool minimised, bool showTaskbarIcon = true, bool disabled = false, bool mouseInput = true, bool kbInput = true ) = 0;
+	virtual void SwapBuffers( VPANEL panel ) = 0;
+	virtual void Invalidate( VPANEL panel ) = 0;
+	virtual void SetCursor( HCursor cursor ) = 0;
+	virtual bool IsCursorVisible( void ) = 0;
+	virtual void ApplyChanges( void ) = 0;
+	virtual bool IsWithin( int x, int y ) = 0;
+	virtual bool HasFocus( void ) = 0;
+	virtual bool SupportsFeature( SurfaceFeature_e feature ) = 0;
+	virtual void RestrictPaintToSinglePanel( VPANEL panel ) = 0;
+	virtual void SetModalPanel( VPANEL panel ) = 0;
+	virtual VPANEL GetModalPanel( void ) = 0;
+	virtual void UnlockCursor( void ) = 0;
+	virtual void LockCursor( void ) = 0;
+	virtual void SetTranslateExtendedKeys( bool state ) = 0;
+	virtual VPANEL GetTopmostPopup( void ) = 0;
+	virtual void SetTopLevelFocus( VPANEL panel ) = 0;
+	virtual HFont CreateFont( void ) = 0;
+	virtual bool AddGlyphSetToFont( HFont font, const char *windowsFontName, int tall, int weight, int blur, int scanlines, int flags ) = 0;
+	virtual bool AddCustomFontFile( const char *fontFileName ) = 0;
+	virtual int GetFontTall( HFont font ) = 0;
+	virtual int GetFontAscent( HFont font, wchar_t wch ) = 0;
+	virtual bool IsFontAdditive( HFont font ) = 0;
+	virtual void GetCharABCwide( HFont font, int ch, int &a, int &b, int &c ) = 0;
+	virtual int GetCharacterWidth( HFont font, int ch ) = 0;
+	virtual void GetTextSize( HFont font, const wchar_t *text, int &wide, int &tall ) = 0;
+	virtual VPANEL GetNotifyPanel( void ) = 0;
+	virtual void SetNotifyIcon( VPANEL context, HTexture icon, VPANEL panelToReceiveMessages, const char *text ) = 0;
+	virtual void PlaySound( const char *fileName ) = 0;
+	virtual int GetPopupCount( void ) = 0;
+	virtual VPANEL GetPopup( int index ) = 0;
+	virtual bool ShouldPaintChildPanel( VPANEL childPanel ) = 0;
+	virtual bool RecreateContext( VPANEL panel ) = 0;
+	virtual void AddPanel( VPANEL panel ) = 0;
+	virtual void ReleasePanel( VPANEL panel ) = 0;
+	virtual void MovePopupToFront( VPANEL panel ) = 0;
+	virtual void MovePopupToBack( VPANEL panel ) = 0;
+	virtual void SolveTraverse( VPANEL panel, bool forceApplySchemeSettings = false ) = 0;
+	virtual void PaintTraverse( VPANEL panel ) = 0;
+	virtual void EnableMouseCapture( VPANEL panel, bool state ) = 0;
+	virtual void GetWorkspaceBounds( int &x, int &y, int &wide, int &tall ) = 0;
+	virtual void GetAbsoluteWindowBounds( int &x, int &y, int &wide, int &tall ) = 0;
+	virtual void GetProportionalBase( int &width, int &height ) = 0;
+	virtual void GetHDProportionalBase(int& width, int& height) = 0;
+	virtual void SetProportionalBase(int width, int height) = 0;
+	virtual void SetHDProportionalBase(int width, int height) = 0;
+	virtual void CalculateMouseVisible( void ) = 0;
+	virtual bool NeedKBInput( void ) = 0;
+	virtual bool HasCursorPosFunctions( void ) = 0;
+	virtual void SurfaceGetCursorPos( int &x, int &y ) = 0;
+	virtual void SurfaceSetCursorPos( int x, int y ) = 0;
+	virtual void DrawTexturedLine( const Vertex_t &a, const Vertex_t &b ) = 0;
+	virtual void DrawOutlinedCircle( int x, int y, int radius, int segments ) = 0;
+	virtual void DrawTexturedPolyLine( const Vertex_t *p, int n ) = 0;
+	virtual void DrawTexturedSubRect( int x0, int y0, int x1, int y1, float texs0, float text0, float texs1, float text1 ) = 0;
+	virtual void DrawTexturedPolygon( int n, Vertex_t *pVertices ) = 0;
+	virtual const wchar_t *GetTitle( VPANEL panel ) = 0;
+	virtual bool IsCursorLocked( void ) const = 0;
+	virtual void SetWorkspaceInsets( int left, int top, int right, int bottom ) = 0;
+	virtual bool DrawGetUnicodeCharRenderInfo( wchar_t ch, CharRenderInfo &info ) = 0;
+	virtual void DrawRenderCharFromInfo( const CharRenderInfo &info ) = 0;
+	virtual void DrawSetAlphaMultiplier( float alpha ) = 0;
+	virtual float DrawGetAlphaMultiplier( void ) = 0;
+	virtual void SetAllowHTMLJavaScript( bool state ) = 0;
+	virtual void OnScreenSizeChanged( int nOldWidth, int nOldHeight ) = 0;
+	virtual HCursor CreateCursorFromFile( const char *curOrAniFile, const char *pPathID ) = 0;
+	virtual void *DrawGetTextureMatInfoFactory( int id ) = 0;
+	virtual void PaintTraverseEx( VPANEL panel, bool paintPopups = false ) = 0;
+	virtual float GetZPos( void ) const = 0;
+	virtual void SetPanelForInput( VPANEL vpanel ) = 0;
+	virtual void DrawFilledRectFade( int x0, int y0, int x1, int y1, unsigned int alpha0, unsigned int alpha1, bool bHorizontal ) = 0;
+	virtual void DrawSetTextureRGBAEx( int id, const unsigned char *rgba, int wide, int tall, int imageFormat ) = 0;
+	virtual void DrawSetTextScale( float sx, float sy ) = 0;
+	virtual bool SetBitmapFontGlyphSet( HFont font, const char *windowsFontName, float scalex, float scaley, int flags ) = 0;
+	virtual bool AddBitmapFontFile( const char *fontFileName ) = 0;
+	virtual void SetBitmapFontName( const char *pName, const char *pFontFilename ) = 0;
+	virtual const char *GetBitmapFontName( const char *pName ) = 0;
+	virtual void ClearTemporaryFontCache( void ) = 0;
+	virtual IImage *GetIconImageForFullPath( const char *pFullPath ) = 0;
+	virtual void DrawUnicodeString( const wchar_t *pwString, FontDrawType_t drawType ) = 0;
+	virtual void PrecacheFontCharacters( HFont font, wchar_t *pCharacters ) = 0;
+	virtual const char *GetResolutionKey( void ) const = 0;
+	virtual void DeleteTextureByID(int textureId) = 0;
+};
+
+}
+
+#define VGUI_SURFACE2_INTERFACE_VERSION "VGUI_Surface2_001"
+
+#endif
