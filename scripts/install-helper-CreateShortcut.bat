499ab38a99e41ed6cd29c2fd99e6b8cc344452f2
hzqst <113660872@qq.com>
2024-02-14 15:18:35 +0800

    Add "-steam" to shortcut in case game searches for master servers and get process hang-up while playing.

--- scripts/install-helper-CreateShortcut.bat
+++ scripts/install-helper-CreateShortcut.bat
@@ -2,7 +2,7 @@
 
 cd /d "%SolutionDir%"
 
-powershell $shell = New-Object -ComObject WScript.Shell;$shortcut = $shell.CreateShortcut(\"MetaHook for %ShortGameName%.lnk\");$shortcut.TargetPath = \"%GameDir%\%LauncherExe%\";$shortcut.WorkingDirectory = \"%GameDir%\";$shortcut.Arguments = \"-insecure -game %LauncherMod%\";$shortcut.Save();
+powershell $shell = New-Object -ComObject WScript.Shell;$shortcut = $shell.CreateShortcut(\"MetaHook for %ShortGameName%.lnk\");$shortcut.TargetPath = \"%GameDir%\%LauncherExe%\";$shortcut.WorkingDirectory = \"%GameDir%\";$shortcut.Arguments = \"-insecure -steam -game %LauncherMod%\";$shortcut.Save();
 
 echo -----------------------------------------------------
 echo Please launch game from shortcut "MetaHook for %ShortGameName%"
