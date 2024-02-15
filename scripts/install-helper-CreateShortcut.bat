c3c0070c3f4570a68f7d5673b689ad74bd71957d
hzqst <113660872@qq.com>
2024-02-15 21:43:13 +0800

    1

--- scripts/install-helper-CreateShortcut.bat
+++ scripts/install-helper-CreateShortcut.bat
@@ -2,15 +2,8 @@
 
 cd /d "%SolutionDir%"
 
-if exist "%SolutionDir%Build\MetaHook.exe" ( 
+powershell $shell = New-Object -ComObject WScript.Shell;$shortcut = $shell.CreateShortcut(\"MetaHook for %ShortGameName%.lnk\");$shortcut.TargetPath = \"%GameDir%\%LauncherExe%\";$shortcut.WorkingDirectory = \"%GameDir%\";$shortcut.Arguments = \"-insecure -game %LauncherMod%\";$shortcut.Save();
 
-    powershell $shell = New-Object -ComObject WScript.Shell;$shortcut = $shell.CreateShortcut(\"MetaHook for %ShortGameName%.lnk\");$shortcut.TargetPath = \"%GameDir%\%LauncherExe%\";$shortcut.WorkingDirectory = \"%GameDir%\";$shortcut.Arguments = \"-insecure -steam -game %LauncherMod%\";$shortcut.Save();
-
-) else (
-   
-    powershell $shell = New-Object -ComObject WScript.Shell;$shortcut = $shell.CreateShortcut(\"MetaHook for %ShortGameName%.lnk\");$shortcut.TargetPath = \"%GameDir%\%LauncherExe%\";$shortcut.WorkingDirectory = \"%GameDir%\";$shortcut.Arguments = \"-insecure -game %LauncherMod%\";$shortcut.Save();
-
-)
 echo -----------------------------------------------------
 echo Please launch game from shortcut "MetaHook for %ShortGameName%"
 
