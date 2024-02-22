9e600cd73b85934c75b7cafd30036a4d03b6d76c
hzqst <113660872@qq.com>
2024-02-22 21:43:17 +0800

    update build-Plugins.bat

--- scripts/build-Plugins.bat
+++ scripts/build-Plugins.bat
@@ -33,6 +33,7 @@ if exist "%InstallDir%\Common7\Tools\vsdevcmd.bat" (
     MSBuild.exe MetaHook.sln "/target:Plugins\StudioEvents" /p:Configuration="Release" /p:Platform="Win32" || goto builderror
     MSBuild.exe MetaHook.sln "/target:Plugins\ThreadGuard" /p:Configuration="Release" /p:Platform="Win32" || goto builderror
     MSBuild.exe MetaHook.sln "/target:Plugins\ResourceReplacer" /p:Configuration="Release" /p:Platform="Win32" || goto builderror
+    MSBuild.exe MetaHook.sln "/target:Plugins\SCCameraFix" /p:Configuration="Release" /p:Platform="Win32" || goto builderror
     goto endbuild
 
 :builderror
