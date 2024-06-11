3460720c9b6dba3b0ba90cb27cd2c44b80a108ea
hzqst <113660872@qq.com>
2024-06-11 22:24:33 +0800

    1

--- Plugins/VGUI2Extension/exportfuncs.cpp
+++ Plugins/VGUI2Extension/exportfuncs.cpp
@@ -419,23 +419,3 @@ void InitWin32Stuffs(void)
 	g_MainWndProc = (WNDPROC)GetWindowLong(g_MainWnd, GWL_WNDPROC);
 	SetWindowLong(g_MainWnd, GWL_WNDPROC, (LONG)VID_MainWndProc);
 }
-
-void RemoveFileExtension(std::string& filePath)
-{
-	// Find the last occurrence of '.'
-	size_t lastDotPosition = filePath.find_last_of(".");
-
-	// Check if the dot is part of a directory component rather than an extension
-	size_t lastPathSeparator = filePath.find_last_of("/\\");
-
-	if (lastDotPosition != std::string::npos) {
-		// Ensure the dot is after the last path separator
-		if (lastPathSeparator != std::string::npos && lastDotPosition < lastPathSeparator) {
-			return; // Dot is part of a directory name, not an extension
-		}
-		// Return the substring from the beginning to the dot
-		filePath = filePath.substr(0, lastDotPosition);
-	}
-
-	// No extension found, return the original path
-}
