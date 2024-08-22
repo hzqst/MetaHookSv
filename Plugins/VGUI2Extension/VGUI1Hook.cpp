abc0c167327e3be18d06af4804f53a289ee6477b
hzqst <113660872@qq.com>
2024-08-22 15:11:22 +0800

    Support non-SDL version engine.

--- Plugins/VGUI2Extension/VGUI1Hook.cpp
+++ Plugins/VGUI2Extension/VGUI1Hook.cpp
@@ -16,6 +16,7 @@
 
 bool ClientVGUI_UseVGUI1();
 bool GameUI_HasExclusiveInput();
+void __fastcall EngineSurfaceWrap_WndProcHandler(void* pthis, int, void* hwnd, unsigned int msg, unsigned int wparam, long lparam);
 void __fastcall EngineSurfaceWrap_AppHandler(void* pthis, int, void* pevent, void *pdata);
 void __fastcall EngineSurfaceWrap_setCursor(void* pthis, int, void *pCursor);
 void __fastcall EngineSurfaceWrap_lockCursor(void* pthis, int);
@@ -27,11 +28,13 @@ HMODULE g_hVGui1 = NULL;
 void* staticEngineSurface = NULL;
 
 static hook_t* g_phook_vgui_TextImage_paint = NULL;
+static hook_t* g_phook_EngineSurfaceWrap_WndProcHandler = NULL;
 static hook_t* g_phook_EngineSurfaceWrap_AppHandler = NULL;
 static hook_t* g_phook_EngineSurfaceWrap_setCursor = NULL;
 static hook_t* g_phook_EngineSurfaceWrap_lockCursor = NULL;
 static hook_t* g_phook_EngineSurfaceWrap_unlockCursor = NULL;
 
+static decltype(EngineSurfaceWrap_WndProcHandler)* m_pfnEngineSurfaceWrap_WndProcHandler = NULL;
 static decltype(EngineSurfaceWrap_AppHandler)* m_pfnEngineSurfaceWrap_AppHandler = NULL;
 static decltype(EngineSurfaceWrap_setCursor)* m_pfnEngineSurfaceWrap_setCursor = NULL;
 static decltype(EngineSurfaceWrap_lockCursor)* m_pfnEngineSurfaceWrap_lockCursor = NULL;
@@ -114,6 +117,94 @@ void __fastcall vgui_TextImage_paint(vgui1_TextImage* pthis, int, void* panel)
 	return gPrivateFuncs.vgui_TextImage_paint(pthis, 0, panel);
 }
 
+class vgui1_EngineSurfaceWrapLegacyProxy : public vgui1_EngineSurfaceWrapLegacy
+{
+public:
+	void* getPanel() override { return nullptr; }
+	void   requestSwap() override { }
+	void   resetModeInfo() override { }
+	int    getModeInfoCount() override { return 0; }
+	bool   getModeInfo(int mode, int& wide, int& tall, int& bpp) override { return false; }
+	void* getApp() override { return nullptr; }
+	void   setEmulatedCursorVisible(bool state) override { }
+	void   setEmulatedCursorPos(int x, int y) override { }
+	void setTitle(const char* title) override { }
+	bool setFullscreenMode(int wide, int tall, int bpp) override { return false; }
+	void setWindowedMode() override { }
+	void setAsTopMost(bool state) override { }
+	void createPopup(void* embeddedPanel) override { }
+	bool hasFocus() override { return false; }
+	bool isWithin(int x, int y) override { return false; }
+	int  createNewTextureID(void) override { return 0; }
+	void addModeInfo(int wide, int tall, int bpp) override { }
+	void drawSetColor(int r, int g, int b, int a) override { }
+	void drawFilledRect(int x0, int y0, int x1, int y1) override { }
+	void drawOutlinedRect(int x0, int y0, int x1, int y1) override { }
+	void drawSetTextFont(void* font) override { }
+	void drawSetTextColor(int r, int g, int b, int a) override { }
+	void drawSetTextPos(int x, int y) override { }
+	void drawPrintText(const char* text, int textLen) override { }
+	void drawSetTextureRGBA(int id, const char* rgba, int wide, int tall) override { }
+	void drawSetTexture(int id) override { }
+	void drawTexturedRect(int x0, int y0, int x1, int y1) override { }
+	void invalidate(void* panel) override { }
+	void enableMouseCapture(bool state) override { }
+	void setCursor(void* cursor) override {
+
+		if (!ClientVGUI_UseVGUI1())
+		{
+			m_pfnEngineSurfaceWrap_setCursor(this, 0, cursor);
+			return;
+		}
+
+		if (GameUI_HasExclusiveInput())
+			return;
+
+		if (g_pVGuiSurface2->IsCursorVisible())
+			return;
+
+		g_pVGuiSurface2->SetVGUI2MouseControl(false);
+		m_pfnEngineSurfaceWrap_setCursor(this, 0, cursor);
+		g_pVGuiSurface2->SetVGUI2MouseControl(true);
+		return;
+	}
+	void swapBuffers() override { }
+	void pushMakeCurrent(void* panel, bool useInsets) override { }
+	void popMakeCurrent(void* panel) override { }
+	void applyChanges() override { }
+	void WndProcHandler(void* hwnd, unsigned int msg, unsigned int wparam, long lparam) override {
+		//VGUI1 SDL event handler, TODO: non-SDL version?
+
+		if (!ClientVGUI_UseVGUI1())
+		{
+			m_pfnEngineSurfaceWrap_WndProcHandler(this, 0, hwnd, msg, wparam, lparam);
+			return;
+		}
+
+		if (GameUI_HasExclusiveInput())
+			return;
+
+		if (g_pVGuiSurface2->IsCursorVisible())
+			return;
+
+		m_pfnEngineSurfaceWrap_WndProcHandler(this, 0, hwnd, msg, wparam, lparam);
+	}
+	void lockCursor() override {
+		//g_pVGuiSurface2->LockCursor();
+	}
+	void unlockCursor() override {
+		//g_pVGuiSurface2->UnlockCursor();
+	}
+	void drawLine(int x1, int y1, int x2, int y2) override { }
+	void drawPolyLine(int* px, int* py, int n) override { }
+	void drawTexturedPolygon(void* pVertices, int n) override { }
+	void drawSetTextureBGRA(int id, const char* rgba, int wide, int tall, int hardwareFilter, int hasAlphaChannel) override { }
+	void drawUpdateRegionTextureBGRA(int nTextureID, int x, int y, const unsigned char* pchData, int wide, int tall) override { }
+	void drawGetTextPos(int& x, int& y) override { }
+	void drawPrintChar(int x, int y, int wide, int tall, float s0, float t0, float s1, float t1) override { }
+	void drawPrintCharAdd(int x, int y, int wide, int tall, float s0, float t0, float s1, float t1) override { }
+};
+
 class vgui1_EngineSurfaceWrapProxy : public vgui1_EngineSurfaceWrap
 {
 public:
@@ -165,85 +256,6 @@ public:
 		m_pfnEngineSurfaceWrap_setCursor(this, 0, cursor);
 		g_pVGuiSurface2->SetVGUI2MouseControl(true);
 		return;
-#if 0
-		auto pCursor = (vgui1_Cursor*)cursor;
-
-		if (pCursor)
-		{
-			switch (pCursor->getDefaultCursor())
-			{
-			case vgui1_Cursor::dc_none:
-			{
-				g_pVGuiSurface2->SetCursor(vgui::CursorCode::dc_none);
-				break;
-			}
-			case vgui1_Cursor::dc_arrow:
-			{
-				g_pVGuiSurface2->SetCursor(vgui::CursorCode::dc_arrow);
-				break;
-			}
-			case vgui1_Cursor::dc_ibeam:
-			{
-				g_pVGuiSurface2->SetCursor(vgui::CursorCode::dc_ibeam);
-				break;
-			}
-			case vgui1_Cursor::dc_hourglass:
-			{
-				g_pVGuiSurface2->SetCursor(vgui::CursorCode::dc_hourglass);
-				break;
-			}
-			case vgui1_Cursor::dc_crosshair:
-			{
-				g_pVGuiSurface2->SetCursor(vgui::CursorCode::dc_crosshair);
-				break;
-			}
-			case vgui1_Cursor::dc_up:
-			{
-				g_pVGuiSurface2->SetCursor(vgui::CursorCode::dc_up);
-				break;
-			}
-			case vgui1_Cursor::dc_sizenwse:
-			{
-				g_pVGuiSurface2->SetCursor(vgui::CursorCode::dc_sizenwse);
-				break;
-			}
-			case vgui1_Cursor::dc_sizenesw:
-			{
-				g_pVGuiSurface2->SetCursor(vgui::CursorCode::dc_sizenesw);
-				break;
-			}
-			case vgui1_Cursor::dc_sizewe:
-			{
-				g_pVGuiSurface2->SetCursor(vgui::CursorCode::dc_sizewe);
-				break;
-			}
-			case vgui1_Cursor::dc_sizens:
-			{
-				g_pVGuiSurface2->SetCursor(vgui::CursorCode::dc_sizens);
-				break;
-			}
-			case vgui1_Cursor::dc_sizeall:
-			{
-				g_pVGuiSurface2->SetCursor(vgui::CursorCode::dc_sizeall);
-				break;
-			}
-			case vgui1_Cursor::dc_no:
-			{
-				g_pVGuiSurface2->SetCursor(vgui::CursorCode::dc_no);
-				break;
-			}
-			case vgui1_Cursor::dc_hand:
-			{
-				g_pVGuiSurface2->SetCursor(vgui::CursorCode::dc_hand);
-				break;
-			}
-			}
-		}
-		else
-		{
-			g_pVGuiSurface2->SetCursor(vgui::CursorCode::dc_user);
-		}
-#endif
 	}
 	void swapBuffers() override { }
 	void pushMakeCurrent(void* panel, bool useInsets) override { }
@@ -283,7 +295,9 @@ public:
 	void drawPrintCharAdd(int x, int y, int wide, int tall, float s0, float t0, float s1, float t1) override { }
 };
 
-vgui1_EngineSurfaceWrapProxy s_EngineSurfaceWrapProxy;
+static vgui1_EngineSurfaceWrapProxy s_EngineSurfaceWrapProxy;
+
+static vgui1_EngineSurfaceWrapLegacyProxy s_EngineSurfaceWrapLegacyProxy;
 
 void VGUI1_SetCursor(vgui1_Scheme::SchemeCursor SchemeCursor)
 {
@@ -329,17 +343,30 @@ void VGUI1_PostInstallHooks(void)
 	if (!staticEngineSurface)
 		return;
 
-	auto pStaticEngineSurface = *(vgui1_EngineSurfaceWrap**)staticEngineSurface;
+	if (gPrivateFuncs.SDL_GetWindowPosition)
+	{
+		auto pStaticEngineSurface = *(vgui1_EngineSurfaceWrap**)staticEngineSurface;
 
-	if (!pStaticEngineSurface)
-		return;
+		if (!pStaticEngineSurface)
+			return;
 
-	PVOID* ProxyVFTable = *(PVOID**)&s_EngineSurfaceWrapProxy;
-	
-	g_phook_EngineSurfaceWrap_setCursor = g_pMetaHookAPI->VFTHook(pStaticEngineSurface, 0, 30, (void*)ProxyVFTable[30], (void**)&m_pfnEngineSurfaceWrap_setCursor);
-	g_phook_EngineSurfaceWrap_AppHandler = g_pMetaHookAPI->VFTHook(pStaticEngineSurface, 0, 35, (void*)ProxyVFTable[35], (void**)&m_pfnEngineSurfaceWrap_AppHandler);
-	//g_phook_EngineSurfaceWrap_lockCursor = g_pMetaHookAPI->VFTHook(pStaticEngineSurface, 0, 36, (void*)ProxyVFTable[36], (void**)&m_pfnEngineSurfaceWrap_lockCursor);
-	//g_phook_EngineSurfaceWrap_unlockCursor = g_pMetaHookAPI->VFTHook(pStaticEngineSurface, 0, 37, (void*)ProxyVFTable[37], (void**)&m_pfnEngineSurfaceWrap_unlockCursor);
+		PVOID* ProxyVFTable = *(PVOID**)&s_EngineSurfaceWrapProxy;
+
+		g_phook_EngineSurfaceWrap_setCursor = g_pMetaHookAPI->VFTHook(pStaticEngineSurface, 0, 30, (void*)ProxyVFTable[30], (void**)&m_pfnEngineSurfaceWrap_setCursor);
+		g_phook_EngineSurfaceWrap_AppHandler = g_pMetaHookAPI->VFTHook(pStaticEngineSurface, 0, 35, (void*)ProxyVFTable[35], (void**)&m_pfnEngineSurfaceWrap_AppHandler);
+	}
+	else
+	{
+		auto pStaticEngineSurfaceLegacy = *(vgui1_EngineSurfaceWrapLegacy**)staticEngineSurface;
+
+		if (!pStaticEngineSurfaceLegacy)
+			return;
+
+		PVOID* ProxyVFTable = *(PVOID**)&s_EngineSurfaceWrapLegacyProxy;
+
+		g_phook_EngineSurfaceWrap_setCursor = g_pMetaHookAPI->VFTHook(pStaticEngineSurfaceLegacy, 0, 29, (void*)ProxyVFTable[29], (void**)&m_pfnEngineSurfaceWrap_setCursor);
+		g_phook_EngineSurfaceWrap_WndProcHandler = g_pMetaHookAPI->VFTHook(pStaticEngineSurfaceLegacy, 0, 34, (void*)ProxyVFTable[34], (void**)&m_pfnEngineSurfaceWrap_WndProcHandler);
+	}
 }
 
 void VGUI1_InstallHooks(void)
@@ -360,8 +387,10 @@ void VGUI1_InstallHooks(void)
 void VGUI1_Shutdown(void)
 {
 	Uninstall_Hook(vgui_TextImage_paint);
+	Uninstall_Hook(EngineSurfaceWrap_WndProcHandler);
+	Uninstall_Hook(EngineSurfaceWrap_AppHandler);
 	Uninstall_Hook(EngineSurfaceWrap_setCursor);
-	Uninstall_Hook(EngineSurfaceWrap_lockCursor);
-	Uninstall_Hook(EngineSurfaceWrap_unlockCursor);
+	//Uninstall_Hook(EngineSurfaceWrap_lockCursor);
+	//Uninstall_Hook(EngineSurfaceWrap_unlockCursor);
 	g_hVGui1 = NULL;
 }
