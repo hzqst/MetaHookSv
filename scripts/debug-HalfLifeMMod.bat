9bdbe39881a71e795d2b38105cb13147a05a6657
hzqst <113660872@qq.com>
2024-10-12 15:15:29 +0800

    add mmod bat

---
+++ scripts/debug-HalfLifeMMod.bat
@@ -0,0 +1,8 @@
+@echo off
+
+set LauncherExe=metahook.exe
+set LauncherMod=HL1MMod
+set FullGameName=Half-Life MMod
+set GameAppId=1761270
+
+call "debug-helper-AIO.bat"
---
+++ scripts/install-to-HalfLifeMMod.bat
@@ -0,0 +1,9 @@
+@echo off
+
+set LauncherExe=metahook.exe
+set LauncherMod=HL1MMod
+set FullGameName=Half-Life MMod
+set ShortGameName=HalfLifeMMod
+set GameAppId=1761270
+
+call "install-helper-AIO.bat"
