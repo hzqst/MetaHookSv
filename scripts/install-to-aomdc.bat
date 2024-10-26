85df7cadbf0f9ed33d5a9440302a2671faf2d4cc
hzqst <113660872@qq.com>
2024-10-26 17:59:15 +0800

    Add aomdc scripts

---
+++ scripts/install-to-aomdc.bat
@@ -0,0 +1,9 @@
+@echo off
+
+set LauncherExe=metahook.exe
+set LauncherMod=aomdc
+set FullGameName=aomdc
+set ShortGameName=aomdc
+set "GameDir=D:\SteamLibrary\steamapps\common\Half-Life"
+
+call "install-helper-AIO.bat"
