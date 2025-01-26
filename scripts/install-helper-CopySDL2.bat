f5a94af726385161dba755a5788b52b3ba537557
hzqst <113660872@qq.com>
2025-01-26 17:47:12 +0800

    update scripts/install-helper-CopySDL2.bat

--- scripts/install-helper-CopySDL2.bat
+++ scripts/install-helper-CopySDL2.bat
@@ -4,15 +4,8 @@ set "GameSDL2Path=%GameDir%\SDL2.dll"
 
 for /f "delims=" %%i in ('powershell.exe -Command "$filePath = '%GameSDL2Path%'; $fileVersion = [System.Diagnostics.FileVersionInfo]::GetVersionInfo($filePath).FileVersion; Write-Output $fileVersion"') do set "GameSDL2_fileVersion=%%i"
 
-if "%GameSDL2_fileVersion%"=="2, 0, 20, 0" (
-    echo SDL2 version is "%GameSDL2_fileVersion%", no need to replace SDL2
-    goto no_replace_sdl2
-)
 
-if "%GameSDL2_fileVersion%"=="2, 0, 16, 0" (
-    echo SDL2 version is "%GameSDL2_fileVersion%", no need to replace SDL2
-    goto no_replace_sdl2
-)
+
 if "%GameSDL2_fileVersion%"==" " (
     echo SDL2 version is empty, no need to replace SDL2
     goto no_replace_sdl2
@@ -22,7 +15,10 @@ if "%GameSDL2_fileVersion%"=="" (
     echo SDL2 version is empty, no need to replace SDL2
     goto no_replace_sdl2
 )
-
+if "%GameSDL2_fileVersion%"=="2, 30, 50, 0" (
+    echo SDL2 version is "%GameSDL2_fileVersion%", no need to replace SDL2
+    goto no_replace_sdl2
+)
 echo SDL2 version is "%GameSDL2_fileVersion%", need to replace SDL2
 copy "%SolutionDir%Build\SDL2.dll" "%GameDir%\" /y
 
