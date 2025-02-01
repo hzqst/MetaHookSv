4ff3d5ac2af3243915b2f807be7a6d5c29b221ec
hzqst <113660872@qq.com>
2025-02-01 16:19:07 +0800

    uses my fork version of SDL2-compat

--- scripts/build-SDL2-compat-x86-Debug.bat
+++ scripts/build-SDL2-compat-x86-Debug.bat
@@ -11,6 +11,6 @@ if not "%SolutionDir:~-1%"=="\" SET "SolutionDir=%SolutionDir%\"
 
 cd /d "%SolutionDir%"
 
-call cmake -S "%SolutionDir%thirdparty\SDL2-compat" -B "%SolutionDir%thirdparty\build\SDL2-compat\x86\Debug" -A Win32 -DCMAKE_INSTALL_PREFIX="%SolutionDir%thirdparty\install\SDL2-compat\x86\Debug" -DCMAKE_PREFIX_PATH="%SolutionDir%thirdparty\install\SDL3\x86\Debug" -DCMAKE_TOOLCHAIN_FILE="%SolutionDir%tools\toolchain.cmake"  -DSDL2COMPAT_TESTS=FALSE -DSDL2COMPAT_INSTALL_SDL3=TRUE
+call cmake -S "%SolutionDir%thirdparty\SDL2-compat-fork" -B "%SolutionDir%thirdparty\build\SDL2-compat-fork\x86\Debug" -A Win32 -DCMAKE_INSTALL_PREFIX="%SolutionDir%thirdparty\install\SDL2-compat-fork\x86\Debug" -DCMAKE_PREFIX_PATH="%SolutionDir%thirdparty\install\SDL3\x86\Debug" -DCMAKE_TOOLCHAIN_FILE="%SolutionDir%tools\toolchain.cmake"  -DSDL2COMPAT_TESTS=FALSE -DSDL2COMPAT_INSTALL_SDL3=TRUE
 
-call cmake --build "%SolutionDir%thirdparty\build\SDL2-compat\x86\Debug" --config Debug --target install
+call cmake --build "%SolutionDir%thirdparty\build\SDL2-compat-fork\x86\Debug" --config Debug --target install
--- scripts/build-SDL2-compat-x86-Release.bat
+++ scripts/build-SDL2-compat-x86-Release.bat
@@ -11,6 +11,6 @@ if not "%SolutionDir:~-1%"=="\" SET "SolutionDir=%SolutionDir%\"
 
 cd /d "%SolutionDir%"
 
-call cmake -S "%SolutionDir%thirdparty\SDL2-compat" -B "%SolutionDir%thirdparty\build\SDL2-compat\x86\Release" -A Win32 -DCMAKE_INSTALL_PREFIX="%SolutionDir%thirdparty\install\SDL2-compat\x86\Release" -DCMAKE_PREFIX_PATH="%SolutionDir%thirdparty\install\SDL3\x86\Release" -DCMAKE_TOOLCHAIN_FILE="%SolutionDir%tools\toolchain.cmake"  -DSDL2COMPAT_TESTS=FALSE -DSDL2COMPAT_INSTALL_SDL3=TRUE
+call cmake -S "%SolutionDir%thirdparty\SDL2-compat-fork" -B "%SolutionDir%thirdparty\build\SDL2-compat-fork\x86\Release" -A Win32 -DCMAKE_INSTALL_PREFIX="%SolutionDir%thirdparty\install\SDL2-compat-fork\x86\Release" -DCMAKE_PREFIX_PATH="%SolutionDir%thirdparty\install\SDL3\x86\Release" -DCMAKE_TOOLCHAIN_FILE="%SolutionDir%tools\toolchain.cmake"  -DSDL2COMPAT_TESTS=FALSE -DSDL2COMPAT_INSTALL_SDL3=TRUE
 
-call cmake --build "%SolutionDir%thirdparty\build\SDL2-compat\x86\Release" --config Release --target install
+call cmake --build "%SolutionDir%thirdparty\build\SDL2-compat-fork\x86\Release" --config Release --target install
