9249858a42fc9b796bfe9bef2e07843a66917c42
hzqst <113660872@qq.com>
2025-02-07 23:03:03 +0800

    always replace SDL2

--- scripts/install-helper-CopySDL2.bat
+++ scripts/install-helper-CopySDL2.bat
@@ -5,20 +5,11 @@ set "GameSDL2Path=%GameDir%\SDL2.dll"
 for /f "delims=" %%i in ('powershell.exe -Command "$filePath = '%GameSDL2Path%'; $fileVersion = [System.Diagnostics.FileVersionInfo]::GetVersionInfo($filePath).FileVersion; Write-Output $fileVersion"') do set "GameSDL2_fileVersion=%%i"
 
 
-
 if "%GameSDL2_fileVersion%"==" " (
     echo SDL2 version is empty, no need to replace SDL2
     goto no_replace_sdl2
 )
 
-if "%GameSDL2_fileVersion%"=="" (
-    echo SDL2 version is empty, no need to replace SDL2
-    goto no_replace_sdl2
-)
-if "%GameSDL2_fileVersion%"=="2, 30, 50, 0" (
-    echo SDL2 version is "%GameSDL2_fileVersion%", no need to replace SDL2
-    goto no_replace_sdl2
-)
 echo SDL2 version is "%GameSDL2_fileVersion%", need to replace SDL2
 copy "%SolutionDir%Build\SDL2.dll" "%GameDir%\" /y
 copy "%SolutionDir%Build\SDL3.dll" "%GameDir%\" /y
