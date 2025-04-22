f4f2f0e11049e96a3f255bdf07a8e97af95b78d1
hzqst <113660872@qq.com>
2025-04-22 21:10:14 +0800

    1

---
+++ include/Interface/IUtilThreadTask.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <interface.h>
+#include <stdint.h>
+
+class IThreadedTask : public IBaseInterface
+{
+public:
+	virtual void Destroy() = 0;
+
+	virtual bool ShouldRun() = 0;
+
+	virtual void Run() = 0;
+};
+
+class IThreadedTaskScheduler : public IBaseInterface
+{
+public:
+	/*
+		Call on game shutdown, to destry all tasks in queue
+	*/
+	virtual void Destroy() = 0;
+
+	/*
+		Call from anywhere
+	*/
+	virtual void QueueTask(IThreadedTask* pTask, bool bQueueToBegin = false) = 0;
+
+	/*
+		Run one task, return true if any task was executed, otherwise false.
+	*/
+
+	virtual bool RunTask() = 0;
+
+	/*
+		Run all tasks in the queue
+	*/
+
+	virtual void RunTasks() = 0;
+};
+
+class IUtilThreadTaskFactory : public IBaseInterface
+{
+public:
+    virtual IThreadedTaskScheduler* CreateThreadedTaskScheduler() = 0;
+};
+
+IUtilThreadTaskFactory* UtilThreadTaskFactory();
+
+#define UTIL_THREAD_TASK_FACTORY_INTERFACE_VERSION "UtilThreadTaskFactory_001"
