2b1c3b20ec1f32d0b368ce935da3d23a039622b3
hzqst <113660872@qq.com>
2025-04-23 12:20:56 +0800

    1

--- scripts/build-Plugins.bat
+++ scripts/build-Plugins.bat
@@ -19,52 +19,41 @@ if exist "%InstallDir%\Common7\Tools\vsdevcmd.bat" (
 
     "%InstallDir%\Common7\Tools\vsdevcmd.bat"
 
-    MSBuild.exe MetaHook.sln "/target:Plugins\Renderer" /p:Configuration="Release" /p:Platform="Win32"
+    MSBuild.exe MetaHook.sln "/target:PluginLibs\UtilThreadTask" /p:Configuration="Release" /p:Platform="Win32"
 
+    MSBuild.exe MetaHook.sln "/target:PluginLibs\UtilHTTPClient_SteamAPI" /p:Configuration="Release" /p:Platform="Win32" 
+
+    MSBuild.exe MetaHook.sln "/target:PluginLibs\UtilHTTPClient_libcurl" /p:Configuration="Release" /p:Platform="Win32"
+
+    MSBuild.exe MetaHook.sln "/target:PluginLibs\UtilAssetsIntegrity" /p:Configuration="Release" /p:Platform="Win32"
+
+    MSBuild.exe MetaHook.sln "/target:Plugins\Renderer" /p:Configuration="Release" /p:Platform="Win32"
 
     MSBuild.exe MetaHook.sln "/target:Plugins\Renderer" /p:Configuration="Release_AVX2" /p:Platform="Win32"
 
-
     MSBuild.exe MetaHook.sln "/target:Plugins\BulletPhysics" /p:Configuration="Release" /p:Platform="Win32"
     
-
     MSBuild.exe MetaHook.sln "/target:Plugins\BulletPhysics" /p:Configuration="Release_AVX2" /p:Platform="Win32"
     
-
     MSBuild.exe MetaHook.sln "/target:Plugins\VGUI2Extension" /p:Configuration="Release" /p:Platform="Win32" 
     
-
     MSBuild.exe MetaHook.sln "/target:Plugins\CaptionMod" /p:Configuration="Release" /p:Platform="Win32"
     
-
     MSBuild.exe MetaHook.sln "/target:Plugins\CommunicationDemo" /p:Configuration="Release" /p:Platform="Win32"
     
-
-    MSBuild.exe MetaHook.sln "/target:Plugins\DontFlushSoundCache" /p:Configuration="Release" /p:Platform="Win32"
-    
-
     MSBuild.exe MetaHook.sln "/target:Plugins\PrecacheManager" /p:Configuration="Release" /p:Platform="Win32"
     
-
     MSBuild.exe MetaHook.sln "/target:Plugins\SCModelDownloader" /p:Configuration="Release" /p:Platform="Win32"
     
-
     MSBuild.exe MetaHook.sln "/target:Plugins\SteamScreenshots" /p:Configuration="Release" /p:Platform="Win32"
-    
 
     MSBuild.exe MetaHook.sln "/target:Plugins\StudioEvents" /p:Configuration="Release" /p:Platform="Win32"
-    
 
     MSBuild.exe MetaHook.sln "/target:Plugins\ThreadGuard" /p:Configuration="Release" /p:Platform="Win32"
-    
 
     MSBuild.exe MetaHook.sln "/target:Plugins\ResourceReplacer" /p:Configuration="Release" /p:Platform="Win32"
-    
 
     MSBuild.exe MetaHook.sln "/target:Plugins\HeapPatch" /p:Configuration="Release" /p:Platform="Win32"
-    
 
     MSBuild.exe MetaHook.sln "/target:Plugins\SCCameraFix" /p:Configuration="Release" /p:Platform="Win32"
-    
-
 )
