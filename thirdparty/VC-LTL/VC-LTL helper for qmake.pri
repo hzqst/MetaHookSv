09c59884af4e2a38645610c2adf5378973a49f39
hzqst <113660872@qq.com>
2025-09-28 14:16:38 +0800

    Add VC-LTL

---
+++ thirdparty/VC-LTL/VC-LTL helper for qmake.pri
@@ -0,0 +1,77 @@
+﻿Release {
+
+# 作者：GPBeta（https://github.com/GPBeta），mingkuang（https://github.com/mingkuang-Chuyu）
+# 修改日期：2021-08-20
+#
+#
+# VC-LTL自动化加载配置，建议你将此文件单独复制到你的工程再使用，该文件能自动识别当前环境是否存在VC-LTL，并且自动应用。
+#
+# 使用方法：
+#   1. 在自己的pro文件中添加“include("VC-LTL helper for qmake.pri")”。
+#
+# VC-LTL默认搜索顺序
+#   1. VC-LTL helper for qmake.pri所在目录，即 $$PWD
+#   2. VC-LTL helper for qmake.pri所在根目录下的VC-LTL目录，即 $$PWD/VC-LTL
+#   3. VC-LTL helper for qmake.pri所在父目录，即$$PWD/..
+#   4. 工程文件(*.pro)所在目录，即 $$_PRO_FILE_PWD_
+#   5. 工程文件(*.pro)所在根目录下的VC-LTL目录，即，即 $$_PRO_FILE_PWD_/..
+#   6. 注册表HKEY_CURRENT_USER\Code\VC-LTL@Root
+#
+# 把VC-LTL放在其中一个位置即可，VC-LTL就能被自动引用。
+#
+# 如果你对默认搜索顺序不满，你可以修改此文件。你也可以直接指定VC_LTL_Root环境变量更加任性的去加载VC-LTL。
+#
+
+
+
+
+#  ---------------------------------------------------------------------VC-LTL设置---------------------------------------------------------------------
+
+# 控制最小兼容系统版本，目前可用版本为 6.0.6000.0（默认）    6.2.9200.0     10.0.10240.0    10.0.19041.0
+# 注意：VC-LTL依赖YY-Thunks，否则可能无法兼容早期系统。如果需要支持Windows XP，该值必须为5.1.2600.0。
+# WindowsTargetPlatformMinVersion = 10.0.10240.0
+
+# VC-LTL使用的CRT模式，SupportLTL可能值为：
+#   * false：禁用VC_LTL
+#   * true：默认值，让VC-LTL自动适应。当最小兼容版本>=10.0时使用ucrt模式，其他系统使用msvcrt模式。
+#   * msvcrt：使用msvcrt.dll作为CRT。注意：msvcrt模式可能不完全支持所有ucrt的新功能。比如setloacl不支持UTF8。
+#   * ucrt：使用ucrtbase.dll作为CRT。注意：早期系统可能需要下载VC-LTL.Redist.Dlls.zip，感谢msvcr14x项目提供兼容XP系统的ucrtbase.dll。
+# 如果需要兼容XP时也使用ucrt，请指定 SupportLTL=ucrt。
+# SupportLTL=ucrt
+
+# -----------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+!isEmpty(VC_LTL_Root) {
+# pri文件根目录存在VC-LTL？
+} else:exists($$PWD/_msvcrt.h) {
+    VC_LTL_Root = $$PWD
+# pri文件根目录下存在VC-LTL？
+} else:exists($$PWD/VC-LTL/_msvcrt.h) {
+    VC_LTL_Root = $$PWD/VC-LTL
+# pri文件父目录存在VC-LTL？
+} else:exists($$PWD/../_msvcrt.h) {
+    VC_LTL_Root = $$PWD/..
+# pri文件父目录存在VC-LTL？
+} else:exists($$PWD/../VC-LTL/_msvcrt.h) {
+    VC_LTL_Root = $$PWD/../VC-LTL
+# pro文件根目录下存在VC-LTL？
+} else:exists($$_PRO_FILE_PWD_/VC-LTL/_msvcrt.h) {
+    VC_LTL_Root = $$_PRO_FILE_PWD_/VC-LTL
+# pro文件父目录存在VC-LTL？
+} else:exists($$_PRO_FILE_PWD_/../VC-LTL/_msvcrt.h) {
+    VC_LTL_Root = $$_PRO_FILE_PWD_/../VC-LTL
+# 读取注册表 HKCU\Code\VC-LTL@Root
+} else:greaterThan(QT_VERSION, 5.12.0) {
+    VC_LTL_Root = $$read_registry(HKCU, Code\\VC-LTL\\Root)
+# QT版本过低，读注册表只能采用reg query
+} else {
+    VC_LTL_Root = $$system(reg query HKCU\\Code\\VC-LTL /v Root, blob)
+	VC_LTL_Root ~= s/.*(\\w:.*)/\\1
+	VC_LTL_Root = $$replace(VC_LTL_Root, \r, "")
+    VC_LTL_Root = $$replace(VC_LTL_Root, \n, "")
+}
+
+!isEmpty(VC_LTL_Root): include($$VC_LTL_Root/config/config.pri)
+
+}
---
+++ thirdparty/VC-LTL/config/config.cmd
@@ -0,0 +1,177 @@
+::@echo off
+::
+::  请不要直接使用此脚本，应该使用VC-LTL helper for nmake.cmd
+::
+
+call:InitMuiStrings
+
+
+if /i "%VC_LTL_Helper_Load%" == "true" goto:eof
+
+set InternalSupportLTL=%SupportLTL%
+if /i "%InternalSupportLTL%" == "false" goto:eof
+if /i "%InternalSupportLTL%" == "" (set InternalSupportLTL=true)
+
+set "VC_LTL_Root=%~dp0"
+set "VC_LTL_Root=%VC_LTL_Root:~0,-7%"
+
+if "%INCLUDE%" == "" echo %ERROR_VC_LTL_CANNOT_FOUND_INCLUDE_ENV%&&goto:eof
+
+if "%LIB%" == "" echo %ERROR_VC_LTL_CANNOT_FOUND_LIB_ENV%&&goto:eof
+
+
+
+if "%VisualStudioVersion%" == "14.0" goto CheckPlatform
+if "%VisualStudioVersion%" == "15.0" goto CheckPlatform
+if "%VisualStudioVersion%" == "16.0" goto CheckPlatform
+if "%VisualStudioVersion%" == "17.0" goto CheckPlatform
+
+echo %ERROR_VC_LTL_NOT_SUPPORT_PLATFORM_TOOLSET%&&goto:eof
+
+:CheckPlatform
+
+if /i "%Platform%" == "" goto Start_VC_LTL
+
+if /i "%Platform%" == "x86" goto Start_VC_LTL
+
+if /i "%Platform%" == "x64" goto Start_VC_LTL
+
+if /i "%Platform%" == "arm" goto Start_VC_LTL
+
+if /i "%Platform%" == "arm64" goto Start_VC_LTL
+
+echo %ERROR_VC_LTL_NOT_SUPPORT_PLATFORM%
+
+goto:eof
+
+
+:Start_VC_LTL
+
+::VC-LTL核心版本号，由于4.X并不兼容3.X。此值可以用于兼容性判断。
+set LTL_CoreVersion=5
+
+set VC_LTL_Helper_Load=true
+
+set LTLPlatform=%Platform%
+
+if "%LTLPlatform%" == "" set LTLPlatform=Win32
+if /i "%LTLPlatform%" == "x86" set LTLPlatform=Win32
+if /i "%LTLPlatform%" == "arm64" set InternalSupportLTL=ucrt
+
+call:FoundBestTargetPlatform
+
+if /i "%InternalSupportLTL%" == "true" if "%InternalLTLCRTVersion:~0,3%"=="10." (set InternalSupportLTL=ucrt)
+if /i "%InternalSupportLTL%" == "true" (set InternalSupportLTL=msvcrt)
+
+if /i "%InternalSupportLTL%" == "msvcrt" if "%InternalLTLCRTVersion:~0,3%"=="10." (set InternalLTLCRTVersion=6.2.9200.0)
+if /i "%InternalSupportLTL%" == "ucrt" if not "%InternalLTLCRTVersion:~0,3%"=="10." (set InternalLTLCRTVersion=10.0.10240.0)
+
+if not exist "%VC_LTL_Root%TargetPlatform\%InternalLTLCRTVersion%\lib\%LTLPlatform%" echo %ERROR_VC_LTL_FILE_MISSING%&&goto:eof
+
+
+
+echo #######################################################################
+echo #                                                                     #
+echo # 8b           d8  ,ad8888ba,         88     888888888888 88          #
+echo # `8b         d8' d8"'    `"8b        88          88      88          #
+echo #  `8b       d8' d8'                  88          88      88          #
+echo #   `8b     d8'  88                   88          88      88          #
+echo #    `8b   d8'   88          aaaaaaaa 88          88      88          #
+echo #     `8b d8'    Y8,         """""""" 88          88      88          #
+echo #      `888'      Y8a.    .a8P        88          88      88          #
+echo #       `8'        `"Y8888Y"'         88888888888 88      88888888888 #
+echo #                                                                     #
+echo #######################################################################
+
+echo VC-LTL Path : %VC_LTL_Root%
+echo VC Tools Version : %VCToolsVersion%
+echo %InternalSupportLTL% Mode : %InternalLTLCRTVersion%
+echo Platform : %LTLPlatform%
+
+
+
+::修改Include
+set INCLUDE=%VC_LTL_Root%TargetPlatform\header;%VC_LTL_Root%TargetPlatform\%InternalLTLCRTVersion%\header;%INCLUDE%
+set LIB=%VC_LTL_Root%TargetPlatform\%InternalLTLCRTVersion%\lib\%LTLPlatform%;%LIB%
+
+goto:eof
+
+
+::搜索最佳TargetPlatform
+:FoundBestTargetPlatform
+
+set InternalLTLCRTVersion=%WindowsTargetPlatformMinVersion%
+
+if "%InternalLTLCRTVersion%" == "" goto FoundBestTargetPlatformDefault
+
+for /f "tokens=3 delims=." %%i in ('echo %InternalLTLCRTVersion%') do set LTLWindowsTargetPlatformMinVersionBuild=%%i
+
+if "%LTLWindowsTargetPlatformMinVersionBuild%" == "" goto FoundBestTargetPlatformDefault
+
+if %LTLWindowsTargetPlatformMinVersionBuild% GEQ 19041 set InternalLTLCRTVersion=10.0.19041.0&&goto:eof
+
+if %LTLWindowsTargetPlatformMinVersionBuild% GEQ 10240 set InternalLTLCRTVersion=10.0.10240.0&&goto:eof
+if /i "%LTLPlatform%" == "arm64" set InternalLTLCRTVersion=10.0.10240.0&&goto:eof
+
+if %LTLWindowsTargetPlatformMinVersionBuild% GEQ 9200 set InternalLTLCRTVersion=6.2.9200.0&&goto:eof
+if /i "%LTLPlatform%" == "arm" set InternalLTLCRTVersion=6.2.9200.0&&goto:eof
+
+if %LTLWindowsTargetPlatformMinVersionBuild% GEQ 6000 set InternalLTLCRTVersion=6.0.6000.0&&goto:eof
+
+if /i "%LTLPlatform%" == "x64" set InternalLTLCRTVersion=5.2.3790.0&&goto:eof
+
+::兜底
+set InternalLTLCRTVersion=5.1.2600.0
+
+goto:eof
+
+:FoundBestTargetPlatformDefault
+
+if /i "%LTLPlatform%" == "arm64" set InternalLTLCRTVersion=10.0.10240.0&&goto:eof
+
+if /i "%LTLPlatform%" == "arm" set InternalLTLCRTVersion=6.2.9200.0&&goto:eof
+
+::兼容以前的XP模式
+if /i "%SupportWinXP%" NEQ "true" set InternalLTLCRTVersion=6.0.6000.0&&goto:eof
+
+if /i "%LTLPlatform%" == "x64" set InternalLTLCRTVersion=5.2.3790.0&&goto:eof
+
+set InternalLTLCRTVersion=5.1.2600.0
+
+goto:eof
+
+::获取当前活跃的代码页，用于识别语言环境
+:GetCodePage
+for /f "tokens=*" %%s in ('chcp') do set __codepage__=%%s
+
+
+:TryFindCodePage
+::为空，那么跳出循环
+if "%__codepage__%" == "" goto:eof
+
+echo %__codepage__% | findstr "^[0-9]" > nul
+
+::匹配到只包含数字的，说识别成功，退出循环
+if %ERRORLEVEL% == 0 goto:eof
+
+::删除第一个字符串
+set "__codepage__=%__codepage__:~1%"
+
+goto:TryFindCodePage
+
+goto:eof
+
+
+::初始化多国语言资源
+:InitMuiStrings
+
+call:GetCodePage
+
+set __LangID__=1033
+if "%__codepage__%" == "936" (set __LangID__=2052)
+
+
+call "%~dp0%__LangID__%\config.strings.cmd"
+
+
+goto:eof
