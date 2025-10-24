6b5e918368cc4a707c5555496011aed806a8372b
Dr.Abc <me@drabc.net>
2025-10-24 20:02:48 +0800

    use avalonia aot installer instead of wp

---
+++ toolsrc/MetahookInstaller/MetahookInstallerAvalonia/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace MetahookInstallerAvalonia.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ toolsrc/MetahookInstaller/MetahookInstallerAvalonia/Views/MainWindow.axaml.cs
@@ -0,0 +1,31 @@
+﻿using Avalonia.Controls;
+using Avalonia.Input;
+
+namespace MetahookInstallerAvalonia.Views;
+
+public partial class MainWindow : Window
+{
+
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void TitleBar_PointerPressed(object? sender, PointerPressedEventArgs e)
+    {
+        if (e.GetCurrentPoint(this).Properties.IsLeftButtonPressed)
+        {
+            BeginMoveDrag(e);
+        }
+    }
+
+    private void MinimizeButton_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        WindowState = WindowState.Minimized;
+    }
+
+    private void CloseButton_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        Close();
+    }
+}
