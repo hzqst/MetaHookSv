91da325d58223bb9e68bbce198d0d3ead89d44a0
hzqst <113660872@qq.com>
2026-06-27 20:27:33 +0800

    Add field intensity scripts.

---
+++ scripts/debug-FieldIntensity.bat
@@ -0,0 +1,9 @@
+@echo off
+
+set LauncherExe=metahook.exe
+set LauncherMod=field_intensity
+set FullGameName=Half-Life : Field Intensity
+set ShortGameName=FieldIntensity
+set GameAppId=70
+
+call "debug-helper-AIO.bat"
